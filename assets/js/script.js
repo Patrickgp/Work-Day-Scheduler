ee44dcf579c0cf9270bcf39d12d62144d61540aa
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-06-25 08:44:00 -0400

    created text entry and adding classes to display past, present, and future tasks. Working on saving input to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,40 @@
 // GIVEN I am using a daily planner to create a schedule
+let currentHour = moment().format("HH");
 
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
-
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
+$(document).ready(function () {
+    let now = moment().format("dddd, MMMM Do YYYY, hh:mm a");
+    let displayToday = document.getElementById("currentDay");
+    displayToday.innerHTML = now;
+})
 
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+$(".description").each(function(){
+    var timeStatus = $(this).attr("id").split("-")[1];
 
-// WHEN I click into a time block
-// THEN I can enter an event
+    if (currentHour === timeStatus) {
+        $(this).addClass("present");
+    } else if (currentHour < timeStatus) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    } else if (currentHour > timeStatus) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+    }
+});
 
 // WHEN I click the save button for that time block
 // THEN the text for that event is saved in local storage
+$(".saveBtn").click(function(event){
+    event.preventDefault();
+    var value = $(this).siblings("scheduled-item").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+});
+
 
 // WHEN I refresh the page
 // THEN the saved events persist
+$("#hour-09, scheduled-item").val(localStorage.getItem("09"));

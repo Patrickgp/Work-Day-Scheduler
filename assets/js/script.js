8aa9cf18bb6a93491d573fdcc193d8002d3f2b34
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-06-25 09:45:32 -0400

    fixed data persistence bug. Inputted information now persists through refresh.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // GIVEN I am using a daily planner to create a schedule
 let currentHour = moment().format("HH");
-
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
 $(document).ready(function () {
     let now = moment().format("dddd, MMMM Do YYYY");
     let displayToday = document.getElementById("currentDay");
     displayToday.innerHTML = now;
+
 
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
@@ -36,13 +36,13 @@ $(document).ready(function () {
 
 // WHEN I refresh the page
 // THEN the saved events persist
-    $("#hour-09, scheduled-item").val(localStorage.getItem("09"));
-    $("#hour-10, scheduled-item").val(localStorage.getItem("10"));
-    $("#hour-11, scheduled-item").val(localStorage.getItem("11"));
-    $("#hour-12, scheduled-item").val(localStorage.getItem("12"));
-    $("#hour-13, scheduled-item").val(localStorage.getItem("13"));
-    $("#hour-14, scheduled-item").val(localStorage.getItem("14"));
-    $("#hour-15, scheduled-item").val(localStorage.getItem("15"));
-    $("#hour-16, scheduled-item").val(localStorage.getItem("16"));
-    $("#hour-17, scheduled-item").val(localStorage.getItem("17"));
+    $("#hour-09 .scheduled-item").val(localStorage.getItem("09"));
+    $("#hour-10 .scheduled-item").val(localStorage.getItem("10"));
+    $("#hour-11 .scheduled-item").val(localStorage.getItem("11"));
+    $("#hour-12 .scheduled-item").val(localStorage.getItem("12"));
+    $("#hour-13 .scheduled-item").val(localStorage.getItem("13"));
+    $("#hour-14 .scheduled-item").val(localStorage.getItem("14"));
+    $("#hour-15 .scheduled-item").val(localStorage.getItem("15"));
+    $("#hour-16 .scheduled-item").val(localStorage.getItem("16"));
+    $("#hour-17 .scheduled-item").val(localStorage.getItem("17"));
 });
